e34922a57ebdad8d76b2043e98b4770cdce523c5
FarouqII <farouq006@gmail.com>
2025-09-14 22:14:40 +0300

    Style navbar
    Added a fixed navbar with glassmorphism styling
        - Logo for home navigation
        - Search bar
        - More button for locally stored locations

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./css/reset.css";
-import "./css/main.css";
+import "./css/styles.css";
+import more from "./assets/icons/more.svg";
+import logo from "./assets/icons/logo.svg";
+
+document.getElementById("more").src = more;
+document.getElementById("logo").src = logo;
