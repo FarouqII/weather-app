e975efd8c97f8f303f6ff126771d213e582a2eac
FarouqII <farouq006@gmail.com>
2025-09-20 15:02:22 +0300

    Finalize result page static design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import "./css/reset.css";
 import "./css/styles.css";
 import more from "./assets/icons/more.svg";
 import logo from "./assets/icons/logo.svg";
+import weatherTemp from "./assets/icons/weather/sunny-cloudy.svg";
 
-document.getElementById("more").src = more;
-document.getElementById("logo").src = logo;
+const moreBtn = document.getElementById("more");
+const logoBtn = document.getElementById("logo");
+const weatherImg = document.getElementById("weather-img");
+const tableTiles = document.querySelectorAll(".table-img");
+
+moreBtn.src = more;
+logoBtn.src = logo;
+weatherImg.src = weatherTemp;
+
+for (const tile of tableTiles) tile.src = weatherTemp;
